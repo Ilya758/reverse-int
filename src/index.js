7532549cfc3823bbe33779455a34cad7d4e7d8e0
Ilya758 <subscriber6436@gmail.com>
2021-08-12 00:07:58 +0300

    feat: implement function that reverse the given number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    const reg = /[0]*/g
+
+    if (n % 10 === 0) {
+        return String(n / 10).replace(reg, '').split('').reverse().join('');
+    }
+    return (n < 0) ? String(-n).split('').reverse().join('') : String(n).split('').reverse().join('');
 }
